60cbcc7ac8c6c5cd56496f5d92eb6dcc60a04b50
oscarkayaya <oscarkayaya01@gmail.com>
2026-04-15 14:55:08 -0700

    finished part 1

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
-public class CellPhoneApplication {
+public class CellPhone {
+    private int serialNumber = 0;
+    private String model="t" ;
+    private String Carrier = "ATT";
+    private String phoneNumber = "206 334-4596";
+    public String owner = "";
+
+    public void setModel(String model){
+        this.model = model;
+
+    }
+    public String getModel(){
+        return this.model;
+
+    }
+    public int getserialNumber(){
+        return this.serialNumber;
+    }
+    public void setserialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+    public String getCarrier(){
+        return this.Carrier;
+    }
+    public void setCarrier(String Carrier){
+        this.Carrier = Carrier;
+    }
+    public String getphoneNumber(){
+        return this.phoneNumber;
+
+    }
+    public void setphoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    public void main(){
+        CellPhone phone = new CellPhone();
+        CellPhone phone2 = new CellPhone();
+        int Bread = 2;
+        int PRICE = 0;
+        double PRICE2 = 4.99;
+        String name = "Oscar";
+        CellPhone Phone3 = new CellPhone();
+        Phone3.owner = "Oscar";
+        System.out.println(Phone3.getModel());
+        Phone3.setModel("Iphone");
+        System.out.println(Phone3.getModel());
+        System.out.println(Phone3.getserialNumber());
+        System.out.println(Phone3.getCarrier());
+        System.out.println(Phone3.getphoneNumber());
+
+
+
+
+    }
 }
